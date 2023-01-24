e1e2a8dbc825fc40228aee0f69c366b8107f8c99
VitaliyLenin <vitaliylenin77@gmail.com>
2023-01-24 17:25:22 +0200

    Update statistics.jsx

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -3,12 +3,14 @@ import css from './statistics.module.css';
 
 const Statistics = props => {
   <div className={css.stat_wrap}>
-    <span className={css.stat_title}>Statistics</span>
+    {/* <span className={css.stat_title}>Statistics</span> */}
     <p className={css.stat_text}>Good: {props.good}</p>
     <p className={css.stat_text}>Neutral: {props.neutral}</p>
     <p className={css.stat_text}>Bad: {props.bad}</p>
     <p className={css.stat_text}>Total: {props.total}</p>
-    <p className={css.stat_text}>Positive feedback: {props.percentage}%</p>
+    <p className={css.stat_text}>
+      Positive feedback: {props.positivePercentage} %
+    </p>
   </div>;
 };
 
